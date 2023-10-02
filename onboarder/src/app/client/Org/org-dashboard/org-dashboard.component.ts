4646436d8b5618ea30cff3b6613cd97706f30446
Badbaddie28 <johnchristian.fmagno@gmail.com>
2023-10-02 14:54:46 +0800

    org partially done

--- onboarder/src/app/client/Org/org-dashboard/org-dashboard.component.ts
+++ onboarder/src/app/client/Org/org-dashboard/org-dashboard.component.ts
@@ -1,29 +1,9 @@
-import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
-import { SidebarService } from './../org-navbar/sidebar.service';
-import { Subscription } from "rxjs";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-org-dashboard',
   templateUrl: './org-dashboard.component.html',
   styleUrls: ['./org-dashboard.component.css']
 })
-export class OrgDashboardComponent implements OnDestroy {
-  isSidebarActive = false;
-  private sidebarSubscription: Subscription;
-
-  constructor(
-    private sidebarService: SidebarService,
-    private cdr: ChangeDetectorRef
-    ) {
-    this.sidebarSubscription = this.sidebarService.sidebarState$.subscribe(
-      (state) => {
-        this.isSidebarActive = state;
-        this.cdr.detectChanges();
-      }
-    );
-  }
-
-  ngOnDestroy() {
-    this.sidebarSubscription.unsubscribe();
-  }
+export class OrgDashboardComponent {
 }
--- onboarder/src/app/client/Org/org-navbar/org-navbar.component.ts
+++ onboarder/src/app/client/Org/org-navbar/org-navbar.component.ts
@@ -1,18 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { SidebarService } from './sidebar.service';
 
 @Component({
   selector: 'app-org-navbar',
   templateUrl: './org-navbar.component.html',
   styleUrls: ['./org-navbar.component.css']
 })
-export class OrgNavbarComponent implements OnInit {
-  constructor(private SidebarService: SidebarService) { }
-  
-  toggleSidebar() {
-    this.SidebarService.toggleSidebar();
-  }
-  
-  ngOnInit(): void {
-  }
+export class OrgNavbarComponent  {
+
 }
