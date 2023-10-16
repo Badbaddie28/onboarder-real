8d8a8a995780b168ff4d7718cf63e72c6d9ee1c9
itsglyb <geli.robiso@gmail.com>
2023-10-16 12:47:26 +0800

    delete and ud - member & org
    di pa nakalagay sa (click) si update

--- onboarder/src/app/client/Admin/admin-orgs/admin-orgs.component.ts
+++ onboarder/src/app/client/Admin/admin-orgs/admin-orgs.component.ts
@@ -8,6 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AdminOrgsComponent {
   OrganizationArray : any[] =[];
+  _id = "";
+  orgName = "";
+  orgType = "";
+  email = "";
+  dateCreated: any;
 
   constructor(private http: HttpClient){
     this.getAllOrganization();
@@ -19,6 +24,39 @@ export class AdminOrgsComponent {
     {
       console.log(resultData);
       this.OrganizationArray = resultData;
+    })
+  }
+
+  setUpdate(data:any)
+  {
+    this._id = data._id;
+    this.orgName = data.orgName;
+    this.orgType = data.orgType;
+    this.email = data.email;
+    this.dateCreated = data.dateCreated;
+  }
+
+  updateOrganization(){
+    let orgData = {
+      "Org ID" : this._id,
+      "Org Name" : this.orgName,
+      "Org Type" : this.orgType,
+      "Org Email" : this.email,
+      "Date Created" : this.dateCreated
+    };
+
+    this.http.patch("http://localhost:5000/api/organization" + "/" + this._id, orgData).subscribe((resultData:any)=>
+    {
+      console.log(resultData);
+      this.getAllOrganization();
+    })
+  }
+
+  setDelete(data:any) {
+    this.http.delete("http://localhost:5000/api/organization" + "/" + data._id).subscribe((resultData: any)=>
+    {
+      console.log(resultData);
+      this.getAllOrganization();
     })
   }
 }
