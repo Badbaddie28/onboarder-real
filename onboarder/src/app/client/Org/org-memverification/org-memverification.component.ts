82a3d181e0660c3a0d198bf38a9e2f1793cf125e
kxthhhh <victoriokatherinegail@gmail.com>
2023-11-02 17:54:05 +0800

    verification to member

--- onboarder/src/app/client/Org/org-memverification/org-memverification.component.ts
+++ onboarder/src/app/client/Org/org-memverification/org-memverification.component.ts
@@ -9,6 +9,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class OrgMemverificationComponent {
   membershipApplicationDetails: any[] = [];
+  acceptModalId: string = '';
+  _id="";
+  isVerified = "";
 
 
 constructor(
@@ -34,7 +37,24 @@ constructor(
         console.log(resultData);
         this.membershipApplicationDetails = resultData;
       });
+  }
 
+  setAcceptModalId(id: string): void {
+    this.acceptModalId = id;
+  }
+
+  accept(_id: string): void{
+    const updatedData = { isVerified: true };
+    this.http.patch(`http://localhost:5000/api/membershipApplication/${_id}`, updatedData, { withCredentials: true })
+      .subscribe((response: any) => {
+        // Handle the response as needed, for example, update the UI or show a success message
+        console.log('Application verified successfully:', response);
+        // Optionally, you can reload the updated data after verification
+        this.getAllMembershipApplication();
+      }, (error) => {
+        // Handle error if the PATCH request fails
+        console.error('Error verifying application:', error);
+      });
 
   }
 
